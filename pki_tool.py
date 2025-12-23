54beab8221b24b108532442e171e735e13194145
Suyogya Chaudhary <155903262+suyogyachaudhary@users.noreply.github.com>
2025-12-23 09:41:13 +0545

    Create pki_tool.py

---
+++ pki_tool.py
@@ -0,0 +1,9 @@
+# pki_tool.py
+"""Simple PKI simulator for CI/CD demo."""
+
+def generate_keys():
+    print("Public Key: ABC123-F1A2-B3C4")
+    print("Private Key: XYZ789-S3C4-D5E6")
+
+if __name__ == "__main__":
+    generate_keys()
