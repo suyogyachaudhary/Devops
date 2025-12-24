663b6542e3f788488f27e27148afb82daf0bd51e
Suyogya Chaudhary <155903262+suyogyachaudhary@users.noreply.github.com>
2025-12-24 11:37:45 +0545

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+# app.py
+"""Simple PKI tool for CI/CD demo."""
+
+def generate_keys():
+    """Print fake public and private keys."""
+    print("Public Key: ABC123-F1A2-B3C4")
+    print("Private Key: XYZ789-S3C4-D5E6")
+
+if __name__ == "__main__":
+    generate_keys()
